fb09db95bd6d9d31c97e4a01aecef1adf3003f48
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-22 12:02:51 +0200

    app.js : changed structure and import products

--- backend/app.js
+++ backend/app.js
@@ -6,45 +6,16 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 const api = process.env.API_URL;
+const productsRouter = require('./routers/products')
 
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
+app.use(`${api}/products`, productsRouter)
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
+const Product = require('./models/product');
 
-const Product = mongoose.model('Product', productSchema);
-
-
-app.get(`${api}/products`, async (req, res)=>{
-    const productList = await Product.find();
-    if(!productList) {
-        res.status(500).json({success : false})
-    }
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res)=>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
