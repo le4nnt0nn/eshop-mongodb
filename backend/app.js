fcae0d5c1184d2795fa7f67130d34059ea3460e4
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-22 10:23:25 +0200

    app.js : changed port and added req & res

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server is running http://localhost:3000");
+app.get("/", (req, res)=>{
+    res.send("hello API !");
+})
+
+app.listen(3003, ()=>{
+    console.log("server is running http://localhost:3003");
 })
