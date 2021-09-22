7a23eb56dafc660fc9c137df3fc0c88c545e84f9
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-22 12:17:10 +0200

    app.js : formatted

--- backend/app.js
+++ backend/app.js
@@ -4,31 +4,30 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 require('dotenv/config');
-
 const api = process.env.API_URL;
-const productsRouter = require('./routers/products')
+
+const productsRouter = require('./routers/products');
 
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.use(`${api}/products`, productsRouter)
+//Routers
+app.use(`${api}/products`, productsRouter);
 
-const Product = require('./models/product');
-
-
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'eshop-database'
-})
-.then(()=>{
-    console.log('Database Connection is ready...')
-})
-.catch((err)=>{
+    dbName: 'eshop-database',
+  })
+  .then(() => {
+    console.log('Database Connection is ready...');
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-app.listen(3003, ()=>{
-    console.log('server is running http://localhost:3003');
-})
+app.listen(3003, () => {
+  console.log('server is running http://localhost:3003');
+});
