fa974a2e730fb0298b7f00987b6ce0bd99d39fb8
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-22 10:30:37 +0200

    app.js : added api

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
+require("dotenv/config");
+
+const api = process.env.API_URL;
+
+app.get(api+"/products", (req, res)=>{
     res.send("hello API !");
 })
 
 app.listen(3003, ()=>{
+    console.log(api);
     console.log("server is running http://localhost:3003");
 })
