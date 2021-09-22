27623423ed814fc342443f5d645d554c8bacb25a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-22 11:19:03 +0200

    app.js : added schema and model of Product

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,16 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+
 app.get(`${api}/products`, (req, res)=>{
     const product = {
         id: 1,
@@ -21,9 +31,20 @@ app.get(`${api}/products`, (req, res)=>{
 })
 
 app.post(`${api}/products`, (req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct=> {
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 })
 
 mongoose.connect(process.env.CONNECTION_STRING, {
