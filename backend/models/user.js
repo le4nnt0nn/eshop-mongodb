16c01c02e5a0c144e0e0e9398cf7b29adc30dfa5
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-23 12:40:44 +0200

    user.js : added all fields

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,46 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    street: {
+        type: String,
+        default: '',
+    },
+    apartament: {
+        type: String,
+        default: '',
+    },
+    zip: {
+        type: String,
+        default: '',
+    },
+    city: {
+        type: String,
+        default: '',
+    },
+    country: {
+        type: String,
+        default: '',
+    }
 })
 
 // exporta el Schema con nombre User
