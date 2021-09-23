df6bb9a159ba14ebb9920937f4feb955ee5e18fd
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-23 15:45:35 +0200

    app.js : added erroHandler

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const authJwt = require('./helper/jwt');
+const errorHandler = require('./helper/error-handler')
 
 app.use(cors());
 app.options('*', cors())
@@ -13,6 +14,7 @@ app.options('*', cors())
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use(errorHandler());
 
 //Routers
 const categoriesRouter = require('./routers/categories');
