cbe621cf583a9fffdcaf4ca6df0982001216fb81
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-23 12:59:08 +0200

    users.js : added all fields

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -10,5 +10,26 @@ router.get(`/`, async (req, res)=>{
     res.send(userList);
 })
 
+router.post('/', async (req, res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(404).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+
 // exporta el modulo router
 module.exports = router;
