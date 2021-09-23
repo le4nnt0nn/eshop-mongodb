4fcb1aea229d337a129a59c4b9011ae1141b03be
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-23 13:28:07 +0200

    users.js : added passwordbcrypt

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
 router.get(`/`, async (req, res)=>{
     const userList = await User.find();
@@ -14,7 +15,10 @@ router.post('/', async (req, res)=>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: req.body.passwordHash,
+        /* bycrypt permite encriptar la password
+           pasa de passwordHash a password en el post (OJO)
+        */
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
