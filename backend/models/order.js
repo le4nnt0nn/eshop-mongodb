af4722dde26f2746e5b8f6c5a2deeb919b42aba0
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-27 11:21:37 +0200

    order.js : added full Schema

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,8 +1,60 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
- 
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true,
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    },
 })
+
+/* Cambia el id -> _id to id */
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
+});
 
 // exporta el Schema con nombre Order
 exports.Order = mongoose.model('Order', orderSchema);
