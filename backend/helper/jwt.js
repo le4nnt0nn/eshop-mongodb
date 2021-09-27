4b12c5fef0fafe2d992b577d8037cbff3221014a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-27 10:44:02 +0200

    jwt.js : used api_url

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -2,14 +2,16 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
+    const api = process.env.API_URL;
     return expressJwt({
         secret,
         algorithms: ['HS256']
     }).unless({
         path: [
-            {url: '/api/v1/users/products' , methods: ['GET', 'OPTIONS']},
-            '/api/v1/users/login',
-            '/api/v1/users/register'
+            // Para que el usuario pueda ejecutar GET en products
+            {url: `${api}/products` , methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`,
         ]
     })
 }
