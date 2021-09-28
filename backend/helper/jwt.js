1ece6aaada5acd9b146949bfcf536e1bc4e4de4a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-09-28 11:38:24 +0200

    jwt.js : added new two paths

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -9,10 +9,13 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
+            // Excluye de autenticación la carpeta uploads
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS']},
             // Para que el usuario pueda ejecutar GET en products
             // La expresión regular obtiene todas las variantes de products (ej: featured/3)
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS']},
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]
